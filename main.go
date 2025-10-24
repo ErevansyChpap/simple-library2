6a31238002280591ca1bc32bc1898753c79d8719
ErevansyChpap <mihailcykalov5@gmail.com>
2025-10-24 12:16:37 +0300

    upgrade project all

--- main.go
+++ main.go
@@ -7,37 +7,73 @@ func main() {
 
 	myLibrary := &Library{}
 
-	fmt.Println("\n--- Наполняем библиотеку ---")
-	myLibrary.AddReader("Тамерлан", "Джигкаев")
-	myLibrary.AddReader("Линда", "Элбакянц")
+	fmt.Println("\n--- Наполняю библиотеку ---")
 
-	myLibrary.AddBook(1984, "1984", "Джордж Оруэлл")
-	myLibrary.AddBook(1967, "Мастер и Маргарита", "Михаил Булгаков")
-
-	fmt.Println("\n--- Библиотека готова к работе ---")
-	fmt.Println("Количество читателей:", len(myLibrary.Readers))
-	fmt.Println("Количество книг:", len(myLibrary.Books))
-
-	fmt.Println("---Тестируем выдачу книг---")
-	//Выдаем книгу 1 читателю 1
-	err := myLibrary.IssueBookToReader(1, 1)
-	if err != nil {
-		fmt.Println("Ошибка выдачи", err)
+	reader, err := myLibrary.AddReader("Владимир", "Асеев")
+	if err != nil{
+		fmt.Println(err)
+	} else {
+		fmt.Println("Зарегестрирован новый читатель:", reader)
+	}
+	reader, err = myLibrary.AddReader("Алан", "Кусраев")
+	if err != nil{
+		fmt.Println(err)
+	} else {
+		fmt.Println("Зарегестрирован новый читатель:", reader)
 	}
 
-	//Проверить статус книги после выдачи
-	book, _ := myLibrary.FindBookById(1)
-	if book != nil {
-		fmt.Println("Статус книги после выдачи:", book)
+	b, err := myLibrary.AddBook(1867, "Война и мир", "Лев Толстой")
+	if err != nil{
+		fmt.Println(err)
+	} else {
+		fmt.Printf("Книга (%v) успешно добавлена.", b)
 	}
-
-	//Тестирование возврата книги
-	test := myLibrary.ReturnBook(1)
-	if test != nil {
-		fmt.Println(test)
+	b, err = myLibrary.AddBook(1835, "Мертвые души", "Николай Гоголь")
+	if err != nil{
+		fmt.Println(err)
+	} else {
+		fmt.Printf("\nКнига (%v) успешно добавлена.", b)
 	}
-
-	if book != nil {
-		fmt.Println("Статус книги после возврата:", book)
+	b, err = myLibrary.AddBook(1925, "Собачье сердце", "Михаил Булгаков")
+	if err != nil{
+		fmt.Println(err)
+	} else {
+		fmt.Printf("\nКнига (%v) успешно добавлена.\n", b)
+	}
+	
+	fmt.Println("----Книга успешно выдана----")
+	err = myLibrary.IssueBookToReader(1, 1)
+	if err != nil{
+		fmt.Println(err)
+	} else {
+		fmt.Println(myLibrary.Books[1])
+	}
+	fmt.Println("----Книга уже выдана----")
+	err = myLibrary.IssueBookToReader(1, 1)
+	if err != nil{
+		fmt.Println(err)
+	} else {
+		fmt.Println(myLibrary.Books[1])
+	}
+	fmt.Println("----Такого читателя нет----")
+	err = myLibrary.IssueBookToReader(2, 15)
+	if err != nil{
+		fmt.Println(err)
+	} else {
+		fmt.Println(myLibrary.Books[1])
+	}
+	fmt.Println("----Успешный возврат книги----")
+	err = myLibrary.ReturnBook(1)
+	if err != nil{
+		fmt.Println(err)
+	} else {
+		fmt.Println("Статус книги после возварат:", myLibrary.Books[1])
+	}
+	fmt.Println("----Книга уже в библиотеке----")
+	err = myLibrary.ReturnBook(1)
+	if err != nil{
+		fmt.Println(err)
+	} else {
+		fmt.Println("Статус книги после возварата:", myLibrary.Books[1])
 	}
 }
